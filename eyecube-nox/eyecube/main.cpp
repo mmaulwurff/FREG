aa516082514167fa2a6be9d279b00f614ce8d502
unknown <Alexander@mwl.(none)>
2012-05-28 23:57:08 +0700

    moving blocks

--- eyecube-nox/eyecube/main.cpp
+++ eyecube-nox/eyecube/main.cpp
@@ -23,28 +23,84 @@ const unsigned short height=100;
 enum subs {
 	STONE,
 	SOIL,
-	PLAYER,
-	OTHER_PLAYER
+	DWARF
 };
+enum dirs { NORTH, SOUTH, EAST, WEST, UP, DOWN };
 
 class Block {
+	protected:
 	subs id;
+	bool movable;
 	public:
+	bool Movable() { return movable; }
 	subs Id() { return id; }
-	Block() {}
-	void* operator new(size_t size) {
-		return ::new Block;
-	}
-	void* operator new(size_t size, subs n) {
+	void* operator new(size_t, subs n) {
 		Block * nblock=::new Block;
 		nblock->id=n;
+		nblock->movable=false;
 		return nblock;
+	}
+};
+
+class Dwarf : private Block {
+	int health;
+	public:
+	void *operator new(size_t) {
+		Dwarf *ndwarf=::new Dwarf;
+		ndwarf->id=DWARF;
+		ndwarf->movable=true;
+		ndwarf->health=1;
+		return ndwarf;
 	}
 };
 
 class World {
 	Block *blocks[shred_width*3][shred_width*3][height];
+	Dwarf * playerP;
+	unsigned short playerX, playerY, playerZ;
+	long longitude, latitude;
+	void *CreateBlock(subs id) {
+		switch (id) {
+			case DWARF: return new Dwarf;
+			default: return new (id) Block;
+		}
+	}
 	public:
+	int Move(int i, int j, int k, dirs dir) {
+		if (blocks[i][j][k]==NULL)
+			return 1;
+		else if ( !blocks[i][j][k]->Movable() ||
+				(!i && dir==WEST ) ||
+				(!j && dir==NORTH) ||
+				(!k && dir==DOWN ) ||
+				(i==shred_width*3-1 && dir==EAST ) ||
+				(j==shred_width*3-1 && dir==SOUTH) ||
+				(k==height-1 && dir==UP) )
+			return 0;
+		else {
+			int newi=i, newj=j, newk=k;
+			switch (dir) {
+				case NORTH: --newj; break;
+				case SOUTH: ++newj; break;
+				case EAST:  ++newi; break;
+				case WEST:  --newi; break;
+				case UP:    ++newk; break;
+				case DOWN:  --newk; break;
+			}
+			if ( Move(newi, newj, newk, dir) ) {
+				if (blocks[i][j][k]==(Block*)playerP) {
+					playerX=newi;
+					playerY=newj;
+					playerZ=newk;
+				}
+				Block *temp=blocks[i][j][k];
+				blocks[i][j][k]=blocks[newi][newj][newk];
+				blocks[newi][newj][newk]=temp;
+				return 1;
+			} else return 0;
+		}
+	}
+	void Move(dirs dir) { Move(playerX, playerY, playerZ, dir); }
 	subs Id(int i, int j, int k) { return blocks[i][j][k]->Id(); }
 	int Transparent(int i, int j, int k) {
 		if (blocks[i][j][k]==NULL)
@@ -55,15 +111,22 @@ class World {
 	}
 	World() {//generate new world
 		//TODO: add load and save
+		longitude=0;
+		latitude=0;
 		unsigned short i, j, k;
 		for (i=0; i<shred_width*3; ++i)
 		for (j=0; j<shred_width*3; ++j) {
 			for (k=0; k<height/2; ++k)
-				blocks[i][j][k]=new (STONE) Block;
+				blocks[i][j][k]=(Block*)CreateBlock(STONE);
 			for ( ; k<height; ++k)
 				blocks[i][j][k]=NULL;
 		}
-		blocks[shred_width*2][shred_width*2][(int)height/2]=new (SOIL) Block;
+		playerX=shred_width*2;
+		playerY=shred_width*2;
+		playerZ=height/2;
+		blocks[playerX][playerY][playerZ] =(Block*)( playerP=(Dwarf*)CreateBlock(DWARF) );
+		blocks[shred_width*2-5][shred_width*2-5][height/2]=(Block*)CreateBlock(DWARF);
+
 	}
 	~World() {
 		unsigned short i, j, k;
@@ -76,24 +139,24 @@ class World {
 };
 
 class Screen {
-	short centerX, centerY;
 	World * const w; //connected world
 	char CharName(int i, int j, int k) {
 		switch ( w->Id(i, j, k) ) {
 			case STONE: return '#';
 			case SOIL:  return 's';
+			case DWARF: return '@';
 			default: return '?';
 		}
 	}
 	public:
-	Screen(World *wor) : w(wor), centerX(shred_width*1.5), centerY(shred_width*1.5) {}
+	Screen(World *wor) : w(wor) {}
 	void print() {//real print() will be written with ncurses
 		unsigned short i, j, k;
-		for ( i=0; i<shred_width*3; ++i, printf("\n") )
-		for ( j=0; j<shred_width*3; ++j )
+		for ( j=0; j<shred_width*3; ++j, printf("\n") )
+		for ( i=0; i<shred_width*3; ++i )
 			for (k=height-1; k>=0; --k)
 				if ( !w->Transparent(i, j, k) ) {
-					printf( "%c_", CharName(i, j, k) );
+					printf( "%c", CharName(i, j, k) );
 					break;
 				}
 	}
@@ -102,5 +165,13 @@ class Screen {
 int main() {
 	World earth;
 	Screen screen(&earth);
-	screen.print();
+	char c;
+	while ((c=getchar())!='Q') {
+		switch (c) {
+			case ',': earth.Move(NORTH); screen.print(); break;
+			case 'o': earth.Move(SOUTH); screen.print(); break;
+			case 'e': earth.Move(EAST ); screen.print(); break;
+			case 'a': earth.Move(WEST ); screen.print(); break;
+		}
+	}
 }
